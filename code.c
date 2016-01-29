a24f3586cefd33cd7c43caf0b3b6289e622bed33
Ludvig af Klinteberg <ludvigak@kth.se>
2016-01-29 09:29:17 +0100

    Added Doxygen comments with some math.

--- code.c
+++ code.c
@@ -1,6 +1,14 @@
 #include "code.h"
 #include "math.h"
 
+/**
+ * @brief Compute vector norm.
+ *
+ * Computes the norm of an \f$R^3\f$ vector,
+ * \f$r=\sqrt{x^2 + y^2 + z^2}\f$
+ * @param vec Input vector.
+ * @return The norm \f$r\f$.
+ */
 double R3_vector_norm(const R3_vector* vec)
 {
     double n = sqrt(vec->x*vec->x + vec->y*vec->y * vec->z*vec->z);
--- code.h
+++ code.h
@@ -1,11 +1,16 @@
 #ifndef __CODE_H
 #define __CODE_H
 
+/**
+ * @brief A vector in \f$R^3\f$
+ *
+ * Contains coordinates for x, y and z.
+ */
 typedef struct
 {
-  double x;
-  double y;
-  double z;
+    double x; ///< x coordinate
+    double y; ///< y coordinate
+    double z; ///< z coordinate
 } R3_vector;
 
 double R3_vector_norm(const R3_vector*);

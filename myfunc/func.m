4a76099baec3d6d53af5d3dd2a65003225ea1f99
Ludvig af Klinteberg <ludvigak@kth.se>
2016-01-29 14:54:49 +0100

    Added math to Matlab code documentation

--- myfunc/func.m
+++ myfunc/func.m
@@ -1,5 +1,7 @@
 function [out1, out2] = func(input, mode)
 % Some kind of fancy matlab function.
+% Maybe it computes an integral like this:
+% :math:`\int e^{-x^2} \mathrm{d}x` .
 %
 % :param input: The material
 % :param mode: The method
